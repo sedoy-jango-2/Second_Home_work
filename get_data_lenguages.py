2bc76a4663849cc4a74a36d8277917df1794d161
sedoy-jango-2 <aa18.04.99@gmail.com>
2017-03-14 17:47:19 +0300

    Update get_data_lenguages.py

--- get_data_lenguages.py
+++ get_data_lenguages.py
@@ -10,15 +10,16 @@ else:
     print('Исходный файл существует')
     file_from.seek(0)
     data = json.load(file_from)
-    all_lenguages = ['PHP', ' С', '1С', ' C', '1C', 'Java', 'JavaScript', 'Python', 'Delphi', 'C++', 'С++', 'C#', 'Basic', 'Assembler', 'С#', 'F#', 'R', 'Lisp', 'Objective-C', 'Perl', 'Ruby']
+    all_languages = ['PHP', ' С', '1С', ' C', '1C', 'Java', 'JavaScript', 'Python', 'Delphi', 'C++', 'С++', 'C#', 'Basic', 'Assembler', 'С#', 'F#', 'R', 'Lisp', 'Objective-C', 'Perl', 'Ruby']
     statistic = {}
-    for i in range(len(data)):
-        for j in range(len(all_lenguages)):
-            help_for_lenguages.count_vacancies(data, statistic, all_lenguages, i, j)
-    graphDict = {}
+    for number_of_vacancy in range(len(data)):
+        for language_number in range(len(all_languages)):
+            help_for_lenguages.count_vacancies(data, statistic, all_languages, number_of_vacancy, language_number)
+    graph_dictionary = {}
     help_for_lenguages.print_vacancies(statistic)
-    help_for_lenguages.dict_vacancies(statistic, graphDict)
+    help_for_lenguages.dict_vacancies(statistic, graph_dictionary)
     colors = []
-    for i in range(0, 10):
+    vacancy_count = 10
+    for number_of_vacancy in range(vacancy_count):
         colors.append(random_colors.generate_new_color(colors, pastel_factor=0.8))
-    help_for_lenguages.show_vacancies(graphDict, colors)
+    help_for_lenguages.show_vacancies(graph_dictionary, colors)
