b3df863c6aa5ad78e5b7de90d13a7ac35eeb90f2
sedoy-jango-2 <aa18.04.99@gmail.com>
2017-03-14 17:44:36 +0300

    Update get_jobs_data.py

--- get_jobs_data.py
+++ get_jobs_data.py
@@ -6,9 +6,10 @@ import help_for_get_jobs
 city = input('Введите город в котором Вы хотите работать: ')
 print('Введите ключевые слова')
 keywords = []
-help_for_get_jobs.input_keywords(keywords)
+help_for_get_jobs.input_keywords()
+api_key = input('Введите ключ API')
 print('Запись началась')
-vocation = get_api.getApiFromSj(city, keywords=keywords)
+vocation = get_api.get_jobs_from_sj(city, keywords=keywords, api_key=api_key)
 with open('data_jobs.json', 'w', encoding='utf-8') as outfile:
     json.dump(vocation, outfile, sort_keys=True, indent=4, ensure_ascii=False)
 outfile.close()
